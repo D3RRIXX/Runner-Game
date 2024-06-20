3ea225bd82b15a25cc0060ddacb2df8c0d76ee3a
D3RRIXX <jenyazagevskii@gmail.com>
2024-06-20 18:18:41 +0300

    Added HUD

---
+++ Redux-Test-Assignment/Assets/_Project_/Scripts/UI/FormattedLabel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace CustomExtensions.UIExtensions
+{
+	[RequireComponent(typeof(TextMeshProUGUI))]
+	public class FormattedLabel : MonoBehaviour
+	{
+		[SerializeField] private TextMeshProUGUI _label;
+		[SerializeField, TextArea] private string _format;
+		[SerializeField] private string[] _formatPreviewValues;
+
+		public Color Color
+		{
+			get => _label.color;
+			set => _label.color = value;
+		}
+		
+		public string Format
+		{
+			get => _format;
+			set => _format = value;
+		}
+
+		public void SetValues(params object[] args)
+		{
+			_label.text = string.Format(_format, args);
+		}
+		
+		public void SetValues(object arg)
+		{
+			_label.text = string.Format(_format, arg);
+		}
+		
+		public void SetValues(object arg0, object arg1)
+		{
+			_label.text = string.Format(_format, arg0, arg1);
+		}
+
+		private void Reset()
+		{
+			_label = GetComponent<TextMeshProUGUI>();
+			_format = _label.text;
+		}
+
+		private void OnValidate()
+		{
+			try
+			{
+				if (!Application.isPlaying && _formatPreviewValues != null)
+					_label.text = string.Format(_format, _formatPreviewValues);
+			}
+			catch (FormatException)
+			{
+			}
+		}
+	}
+}
--- Redux-Test-Assignment/Assets/_Project_/Scripts/UI/Screens/HudView.cs
+++ Redux-Test-Assignment/Assets/_Project_/Scripts/UI/Screens/HudView.cs
@@ -1,20 +1,65 @@
-﻿using Game.Levels;
+﻿using System;
+using CustomExtensions.UIExtensions;
+using Game.Events;
+using Game.Levels;
 using Game.Player;
+using Infrastructure.EventBus;
 using Infrastructure.ServiceLocator;
-using TMPro;
+using UniRx;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI.Screens
 {
 	public class HudView : MonoBehaviour
 	{
-		[SerializeField] private TMP_Text _livesLabel;
-		
+		[SerializeField] private FormattedLabel _livesLabel;
+		[SerializeField] private GameObject _invincibleRoot;
+		[SerializeField] private Slider _invincibleSlider;
+
 		private PlayerHealth _player;
+		private IEventService _eventService;
+		private IDisposable _invincibilitySliderStream;
 
 		private void Awake()
 		{
 			_player = AllServices.Container.GetSingle<LevelState>().Player;
+			_eventService = AllServices.Container.GetSingle<IEventService>();
+
+			_eventService.Subscribe<PlayerInvincibilityEvent>(OnPlayerInvincibility);
+		}
+
+		private void Start()
+		{
+			_player.Lives
+			       .Subscribe(x => _livesLabel.SetValues(x.ToString()))
+			       .AddTo(this);
+		}
+
+		private void OnDestroy()
+		{
+			_eventService.Unsubscribe<PlayerInvincibilityEvent>(OnPlayerInvincibility);
+			_invincibilitySliderStream?.Dispose();
+		}
+
+		private void OnPlayerInvincibility(PlayerInvincibilityEvent evt)
+		{
+			if (!evt.IsInvincible)
+			{
+				_invincibleRoot.SetActive(false);
+				_invincibilitySliderStream?.Dispose();
+				_invincibilitySliderStream = null;
+			}
+			else
+			{
+				_invincibleRoot.SetActive(true);
+				_invincibleSlider.maxValue = _invincibleSlider.value = evt.InvincibilityDuration;
+
+				_invincibilitySliderStream = Observable.EveryUpdate()
+				                                       .Scan(evt.InvincibilityDuration, (x, _) => x - Time.deltaTime)
+				                                       .TakeWhile(x => x >= 0f)
+				                                       .Subscribe(x => _invincibleSlider.value = x);
+			}
 		}
 	}
 }
