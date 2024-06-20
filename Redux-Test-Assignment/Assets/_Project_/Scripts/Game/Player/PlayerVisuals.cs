4c4c1d212a339f247b58468629234d6aaaa6f57d
D3RRIXX <jenyazagevskii@gmail.com>
2024-06-20 16:13:26 +0300

    Added shield

--- Redux-Test-Assignment/Assets/_Project_/Scripts/Game/Player/PlayerVisuals.cs
+++ Redux-Test-Assignment/Assets/_Project_/Scripts/Game/Player/PlayerVisuals.cs
@@ -7,60 +7,23 @@ namespace Game.Player
 	[RequireComponent(typeof(PlayerHealth))]
 	public class PlayerVisuals : MonoBehaviour
 	{
-		[SerializeField] private Renderer[] _renderers;
+		[SerializeField] private MeshRenderer _shield;
 		[SerializeField] private PlayerHealth _playerHealth;
 		[SerializeField] private float _blinkInterval = 0.1f;
 
-		private Material _material;
 		private Coroutine _blinkRoutine;
 
 		private void Reset()
 		{
-			_renderers = GetComponentsInChildren<Renderer>();
 			_playerHealth = GetComponent<PlayerHealth>();
 		}
 
 		private void Awake()
 		{
-			_material = _renderers[0].sharedMaterial;
 			_playerHealth.IsInvincible
 			             .SkipLatestValueOnSubscribe()
-			             .Subscribe(b =>
-			             {
-				             if (b)
-				             {
-					             _blinkRoutine = StartCoroutine(BlinkMaterialRoutine());
-				             }
-				             else
-				             {
-					             StopCoroutine(_blinkRoutine);
-					             SetMaterialsColor(Color.white);
-				             }
-			             })
+			             .Subscribe(_shield.gameObject.SetActive)
 			             .AddTo(this);
-		}
-
-		private IEnumerator BlinkMaterialRoutine()
-		{
-			bool isWhite = false;
-			while (true)
-			{
-				Color color = isWhite ? Color.clear : Color.white;
-				
-				SetMaterialsColor(color);
-
-				isWhite = !isWhite;
-
-				yield return new WaitForSeconds(_blinkInterval);
-			}
-		}
-
-		private void SetMaterialsColor(Color color)
-		{
-			foreach (Renderer renderer in _renderers)
-			{
-				renderer.material.color = color;
-			}
 		}
 	}
 }
